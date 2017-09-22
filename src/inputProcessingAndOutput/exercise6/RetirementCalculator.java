7ec2f8ab631ce55ca5ac9a78e7243d3c770d9a8f
Praktikant <Praktikant@iksmobil1103.iks-gmbh.com>
2017-09-22 14:05:07 +0200

    Change package name

---
+++ src/inputProcessingAndOutput/exercise6/RetirementCalculator.java
@@ -0,0 +1,34 @@
+package inputProcessingAndOutput.exercise6;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class RetirementCalculator {
+
+	public static void main(String[] args) {
+
+		RechnungenFürEx6 rechnungen = new RechnungenFürEx6() {
+		};
+		try {
+			Scanner sc = new Scanner(System.in);
+
+			System.out.print("Wie alt sind Sie momentan? --> ");
+			rechnungen.setAlter(sc.nextInt());
+
+			System.out.print("Wann möchten Sie in Rente gehen? --> ");
+			rechnungen.setAbWannRente(sc.nextInt());
+
+			sc.close();
+
+		} catch (NumberFormatException e) {
+			System.out.println("NumberFormatException-Fehler aufgetreten!");
+			System.exit(0);
+			System.out.println("Bitte starten Sie das Programm neu!");
+		} catch (InputMismatchException e) {
+			System.out.println("InputMismatchException-Fehler aufgetreten!");
+			System.out.println("Bitte starten Sie das Programm neu!");
+			System.exit(0);
+		}
+			System.out.println(rechnungen);
+	}
+}
